ff0fc3135b27362dbe0ec32531519566486ae75e
abbos <hikmatullaevabbos24@gmail.com>
2024-07-06 19:23:48 +0500

    cdn added

--- conf/settings.py
+++ conf/settings.py
@@ -126,7 +126,7 @@ AWS_ACCESS_KEY_ID = 'DO00QFKKECEJ66ABV9GD'
 AWS_SECRET_ACCESS_KEY = 'DynSRfM4nLPmigZxiw07x2tw0nt9omPeSuhtGUUHrL8'
 AWS_STORAGE_BUCKET_NAME = 'abbos_socialmedia_cdn'
 AWS_S3_ENDPOINT_URL = 'https://portfolioscdn.blr1.digitaloceanspaces.com'
-AWS_S3_CUSTOM_DOMAIN = 'portfolioscdn.blr1.digitaloceanspaces.com' + AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = 'portfolioscdn.blr1.digitaloceanspaces.com/' + AWS_STORAGE_BUCKET_NAME
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
